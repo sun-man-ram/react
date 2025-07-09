556c1edf9c5447ad6a6b837728680421a5ec19d4
sun-man-ram <manikantasunkara2653@gmail.com>
2025-07-09 09:35:17 +0530

    css in react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Apple from './apple';
 import Sample from './example/Sample';
+import './sample.css'
 function App() {
   return (
     <div className="App">
